58cfa5fc9b46bb0bff1198520dc200066952fde0
Samuel <sgsmultimedia101989@gmail.com>
2023-03-31 08:07:19 -0700

    3-strcmp.c

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* _strcmp - compares two strings
+* @s1: String 1
+* @s2: String 2
+* Return: Resulting string
+*/
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 && *s2 && *s1 == *s2)
+	{
+		s1++;
+		s2++;
+	}
+	return (*s1 - *s2);
+}
